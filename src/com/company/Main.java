2df6a169101668b7c6046c15e8e97329b57f1969
samjo_v <zmsqufxotjd3@gmail.com>
2021-04-20 17:46:23 +0900

    ㅋㅋ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,12 @@ package com.company;
 
 import java.util.Scanner;
 
-class Student{
-    char major, id, name, grade;
+class Student{ //학생정보 클래스 선언
+    char major, id, name, grade; //변수 선언
     int score;
 
 
-    Student(char major, char id, char name, int score) {
+    Student(char major, char id, char name, int score) { //클래스 내의 정보를 담는 메소드 생성
         this.major = major;
         this.id = id;
         this.name = name;
@@ -18,28 +18,27 @@ class Student{
 }
 
 public class Main{
-    static int grade_A=0, grade_B=0, grade_C=0, grade_D=0, grade_F=0, score_sum=0;
-    static double average = 0;
+    static int grade_A=0, grade_B=0, grade_C=0, grade_D=0, grade_F=0, score_sum=0; //각 학점을 카운팅 할 변수 선언
+    static double average = 0; //학생들 점수 평균을 낼 변수
     private boolean retake = false;
 
     public static void main(String[] args){
-        final int MAX = 5;
+        final int MAX = 5; //배열에 사용할 변수 선언
 
-        Student student[] = new Student[MAX];
+        Student student[] = new Student[MAX]; //배열 생성
 
-        Scanner sc = new Scanner(System.in);
-        for(int i=0; i<student.length; i++){
+        Scanner sc = new Scanner(System.in); //값을 입력받을 스캐너 생성
+        for(int i=0; i<student.length; i++){ //각 학생의 정보를 반복으로 입력받는 반복문
             System.out.println("전공, 학번, 이름, 점수를 입력하세요. : ");
             student[i] = new Student(sc.next().charAt(0), sc.next().charAt(0), sc.next().charAt(0), sc.nextInt());
-            gradeCount(student[i]);
-            System.out.println(student[2]);
+            gradeCount(student[i]); //각각 맞는 정보를 입력 받는다.
         }
-        average = score_sum/5;
-        printTap();
+        average = score_sum/5; //평균 계산
+        printTap(); //출력을 하는 메소드 호출
     }
 
 
-    static void gradeCount(Student student){
+    static void gradeCount(Student student){ //학점을 계산해주고 위에서 선언한 학점 카운팅에 더해주는 방식의 메소드.
 
         score_sum += student.score;
         if(student.score>=90) {
@@ -59,11 +58,8 @@ public class Main{
         }
     }
 
-    static void printTap(){
+    static void printTap(){ //학점대로 출력 및 별 출력.
         //System.out.println("* 재수강 대상자는");
-
-
-
         System.out.print("A("+grade_A+"명) |");
         for(int i=0; i<grade_A; i++){
             System.out.print("*");
@@ -89,7 +85,7 @@ public class Main{
             System.out.print("*");
         } System.out.println("");
         System.out.println("----------------------------");
-        System.out.println("총합 5명 (평균 "+average+"점)");
+        System.out.println("총합 5명 (평균 "+average+"점)"); //평균 계산
     }
 }
 
